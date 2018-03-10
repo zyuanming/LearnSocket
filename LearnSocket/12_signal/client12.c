55de684a620e7eb17928a938d6c57dff6dc9d589
zyuanming <zyuanming@outlook.com>
2018-03-10 23:17:29 +0800

    Socket 12: socket 状态, SIGPIPE信号

--- LearnSocket/12_signal/client12.c
+++ LearnSocket/12_signal/client12.c
@@ -1,5 +1,5 @@
 //
-//  server10.c
+//  client10.c
 //  LearnSocket
 //
 //  Created by Zhang Yuanming on 3/8/18.
@@ -15,8 +15,7 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <string.h>
-#include <netdb.h>
-#include <sys/wait.h>
+#include <signal.h>
 
 #define ERR_EXIT(m) \
 do \
@@ -24,8 +23,6 @@ do \
 perror(m); \
 exit(EXIT_FAILURE); \
 } while(0)
-
-
 
 
 ssize_t recv_peak(int sockfd, void *buf, size_t len)
@@ -39,6 +36,27 @@ ssize_t recv_peak(int sockfd, void *buf, size_t len)
     }
 }
 
+// 为了解决粘包问题
+ssize_t writen(int fd, const void *buf, size_t count)
+{
+    size_t nleft = count;
+    ssize_t nwriten;
+    char *bufp = (char *)buf;
+
+    while (nleft > 0) {
+        if ((nwriten = write(fd, bufp, nleft)) < 0) {
+            if (errno == EINTR) {
+                continue;
+            } else if (nwriten == 0) {
+                continue;
+            }
+        }
+        bufp += nwriten;
+        nleft -= nwriten;
+    }
+
+    return count;
+}
 
 // 为了解决粘包问题
 ssize_t readn(int fd, const void *buf, size_t count)
@@ -64,6 +82,7 @@ ssize_t readn(int fd, const void *buf, size_t count)
 
     return count;
 }
+
 
 // 读取一行数据为一条信息,解决粘包问题的一个方法
 ssize_t readline(int sockfd, void *buf, size_t maxline)
@@ -107,66 +126,66 @@ ssize_t readline(int sockfd, void *buf, size_t maxline)
     return -1;
 }
 
-void do_service(int conn) {
-    char recvbuf[1024];
-    int n;
-    while (1) {
-        memset(&recvbuf, 0, sizeof(recvbuf));
-        int ret = readline(conn, recvbuf, 1024);
+
+void echo_cli(int sock) {
+    char sendbuf[1024] = {0};
+    char recvbuf[1024] = {0};
+
+
+    while (fgets(sendbuf, sizeof(sendbuf), stdin) != NULL) {
+
+
+        // writen(sock, sendbuf, strlen(sendbuf));
+
+        // 模拟发生 SIG_PIPE 信号,就是客户端在收到Fin信号后,连续两次发送数据
+        writen(sock, sendbuf, 1);
+        writen(sock, sendbuf + 1, strlen(sendbuf) - 1);
+
+        int ret = readline(sock, recvbuf, sizeof(recvbuf));
         if (ret == -1) {
             ERR_EXIT("readline");
         } else if (ret == 0) {
             printf("client close\n");
             break;
         }
+
         fputs(recvbuf, stdout);
-        write(conn, recvbuf, strlen(recvbuf));
+        printf("reset....");
+        memset(sendbuf, 0, sizeof(sendbuf));
+        memset(sendbuf, 0, sizeof(recvbuf));
     }
+    close(sock);
 }
 
 
 
-
-void handle_sigchld(int sig)
-{
-    // 捕获子进程的状态
-//    wait(NULL);
-
-    // 解决同时多个子进程退出时,丢失信号导致存在僵尸进程
-    while (waitpid(-1, NULL, WNOHANG) > 0) ;
+void handle_sigpipe(int sig) {
+    printf("recv a sig=%d\n", sig);
 }
 
 
-// gcc -Wall -g main.c -o main
-// 观察TCP端口状态:
-// netstat -an | grep tcp | grep 5188
+
+/* gcc -Wall -g main.c -o main
+ 启动服务端和客户端后
+ 通过 ps -ef | grep server 找到服务端进程号
+ 通过 kill 来杀掉 服务端进程,
+ 使用 netstat -an | grep tcp | grep 5188 查看端口详情
+ tcp4       0      0  127.0.0.1.5188         127.0.0.1.55097        FIN_WAIT_2
+ tcp4       0      0  127.0.0.1.55097        127.0.0.1.5188         CLOSE_WAIT
+ tcp4       0      0  127.0.0.1.5188         *.*                    LISTEN
+ 会发现服务端端口处于 FIN_WAIT_2 状态,
+ 客户端端口处于 CLOSE_WAIT状态, 为什么呢?
+ 因为客户端在收到 FIN x ACK 后立即回传了 ACK 后,就没有能够 返回0给服务端(调用close方法),
+ 导致服务端一直处于 FIN_WAIT_2 状态.
+
+ */
 int main(int argc, const char * argv[]) {
 
-    unsigned int x = 0x12345678;
-    unsigned char *p = (unsigned char *)&x;
-    printf("%x %x %x %x\n", p[0], p[1], p[2], p[3]);
-
-    unsigned int y = htonl(x);
-    p = (unsigned char *)&y;
-    printf("%x %x %x %x\n", p[0], p[1], p[2], p[3]);
-
-    in_addr_t addr = inet_addr("192.168.0.100");
-    printf("addr=%d\n", ntohl(addr));
-
-    struct in_addr ipaddr;
-    ipaddr.s_addr = addr;
-    printf("%s\n", inet_ntoa(ipaddr));
-
-
-//    // 1、 忽略这个信号,避免僵尸进程
-//    signal(SIGCHLD, SIG_IGN);
-
-    // 2、
-    signal(SIGCHLD, handle_sigchld);
-
-
-    int listenfd;
-    if ((listenfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+    signal(SIGPIPE, handle_sigpipe);
+//    忽略管道信号
+//    signal(SIGPIPE, SIG_IGN);
+    int sock;
+    if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
         ERR_EXIT("socket");
     }
 
@@ -174,49 +193,33 @@ int main(int argc, const char * argv[]) {
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(5188);
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    inet_aton("127.0.0.1", &servaddr.sin_addr);
 
-    int on = 1;
-    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0) {
-        ERR_EXIT("setsockopt");
+    if (connect(sock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        ERR_EXIT("connect");
     }
 
-    if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-        ERR_EXIT("bind");
+
+    struct sockaddr_in localaddr;
+    socklen_t addrlen = sizeof(localaddr);
+    if (getsockname(sock, (struct sockaddr*)&localaddr, &addrlen) < 0) {
+        ERR_EXIT("getsockname");
     }
 
-    if (listen(listenfd, SOMAXCONN) < 0) {
-        ERR_EXIT("listen");
-    }
+    printf("ip=%s port=%d\n", inet_ntoa(localaddr.sin_addr), ntohs(localaddr.sin_port));
 
-    struct sockaddr_in peeraddr;
-    socklen_t peerlen = sizeof(peeraddr);
-    int conn;
-
-    pid_t pid;
-    while (1) {
-        if ((conn = accept(listenfd, (struct sockaddr *)&peeraddr, &peerlen)) < 0) {
-            ERR_EXIT("accept");
-        }
-
-        printf("ip=%s port=%d\n", inet_ntoa(peeraddr.sin_addr), ntohs(peeraddr.sin_port));
-
-        pid = fork();
-        if (pid == -1) {
-            ERR_EXIT("fork");
-        } else if (pid == 0) {
-            close(listenfd);
-            do_service(conn);
-            exit(EXIT_SUCCESS);
-        } else {
-            close(conn);
-        }
-    }
+    echo_cli(sock);
 
     return 0;
 }
+
+
+
+
+
+
+
+
 
 
 
